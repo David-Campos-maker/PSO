3131c80de21d451c72a60625ed1d47b8e8afcccc
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-06-19 18:42:14 -0300

    Starting project

---
+++ Classes/Particle.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Particle {
+    public double[] Position {get; set;}
+    public double[] BestPosition {get; set;}
+    public double[] Velocity {get; set;}
+}
---
+++ Classes/User.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+
+public class User {
+    public string Name {get; set;}
+    public List<Event> Schedule {get; set;}
+}
