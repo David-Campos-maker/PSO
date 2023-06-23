82fd4e76855e856afd07a79209aba799ed3bd498
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-06-23 18:43:31 -0300

    Testing

--- Classes/PSO.cs
+++ Classes/PSO.cs
@@ -11,39 +11,99 @@ namespace PSO.Classes {
         public double SocialCoefficient {get; set;}
         public double[] GlobalBestPosition {get; set;}
 
-        private double ObjectiveFunction(double[] position, List<Event> events) {
-        double quality = 0;
-
-        for (int i = 0; i < position.Length; i++) {
-            DateTime date = DateTime.FromOADate(position[i]);
-            TimeSpan time = TimeSpan.FromHours(position[i + 1]);
-
+        private bool IsAnyDayAvailableInCurrentYear(List<Event> events) {
+            // Check if there is any day available in the current year for any user
             foreach (Event e in events) {
-                DateTime eventStart = date.Add(time);
-                DateTime eventEnd = eventStart.Add(e.Duration);
-
                 foreach (User user in e.Participants) {
-                    if (user.Schedule.Any(scheduledEvent => 
-                        (scheduledEvent.Date == date && scheduledEvent.Time >= time && scheduledEvent.Time < eventEnd.TimeOfDay) ||
-                        (scheduledEvent.Date == date && scheduledEvent.Time < time && scheduledEvent.Time.Add(scheduledEvent.Duration) > time))) {
-                        quality += 1;
+                    for (int i = 0; i < user.Schedule.Count - 1; i++) {
+                        if (user.Schedule[i].Date.Year == DateTime.Now.Year && user.Schedule[i].Time.Add(user.Schedule[i].Duration) <= user.Schedule[i + 1].Time) {
+                            return true;
+                        }
                     }
                 }
             }
+            return false;
         }
 
-        return quality;
-    }
+        private double ObjectiveFunction(double[] position, List<Event> events) {
+            double quality = 0;
+
+            for (int i = 0; i < position.Length - 1; i += 2) {
+                DateTime date = DateTime.FromOADate(position[i]);
+                TimeSpan time = TimeSpan.FromHours(position[i + 1]);
+
+                // Penalize solutions that assign events to invalid times
+                if (time.Hours < 7 || time.Hours > 18) {
+                    quality += 1000;
+                }
+
+                // Penalize solutions that assign events to invalid dates
+                if (date < DateTime.Now || (date.Year > DateTime.Now.Year && !IsAnyDayAvailableInCurrentYear(events))) {
+                    quality += 1000;
+                }
+
+                foreach (Event e in events) {
+                    DateTime eventStart = date.Add(time);
+                    DateTime eventEnd = eventStart.Add(e.Duration);
+
+                    foreach (User user in e.Participants) {
+                        // Penalize solutions that assign events outside of the user's available schedule
+                        bool isAvailable = false;
+                        for (int j = 0; j < user.Schedule.Count - 1; j++) {
+                            if (user.Schedule[j].Time.Add(user.Schedule[j].Duration) <= time && time.Add(e.Duration) <= user.Schedule[j + 1].Time) {
+                                isAvailable = true;
+                                break;
+                            }
+                        }
+                        if (!isAvailable) {
+                            quality += 1000;
+                        }
+
+                        if (user.Schedule.Any(scheduledEvent => 
+                            (scheduledEvent.Date == date && scheduledEvent.Time >= time && scheduledEvent.Time < eventEnd.TimeOfDay) ||
+                            (scheduledEvent.Date == date && scheduledEvent.Time < time && scheduledEvent.Time.Add(scheduledEvent.Duration) > time))) {
+                            quality += 1;
+                        }
+                    }
+                }
+            }
+
+            return quality;
+        }
 
         public void Run(List<Event> events) {
             // Initialize the particle swarm with random values
             var swarm = new Particle[PopulationSize];
+            var random = new Random();
+            int dimensions = events.Count * 2; // Two dimensions (date and time) for each event
 
             for (int i = 0; i < PopulationSize; i++) {
+                var position = new double[dimensions];
+                var velocity = new double[dimensions];
+
+                // Initialize particle position and velocity with random values
+                for (int j = 0; j < position.Length; j++) {
+                    if (j % 2 == 0) {
+                        // Initialize the event date
+                        double minValue = DateTime.Now.ToOADate();
+                        double maxValue = DateTime.Now.AddYears(1).ToOADate();
+
+                        position[j] = minValue + (maxValue - minValue) * random.NextDouble();
+                        velocity[j] = minValue + (maxValue - minValue) * random.NextDouble();
+                    } else {
+                        // Initialize event time
+                        int minValue = 0;
+                        int maxValue = 23;
+
+                        position[j] = random.Next(minValue, maxValue + 1);
+                        velocity[j] = random.Next(minValue, maxValue + 1);
+                    }
+                }
+
                 swarm[i] = new Particle {
-                    Position = new double[] {},
-                    BestPosition = new double[] {},
-                    Velocity = new double[] {}
+                    Position = position,
+                    BestPosition = position,
+                    Velocity = velocity
                 };
             }
 
@@ -77,11 +137,11 @@ namespace PSO.Classes {
             }
 
             // Assign events to users' schedules using the global best particle position
-            for (int i = 0; i < GlobalBestPosition.Length; i++) {
+            for (int i = 0; i < GlobalBestPosition.Length - 1; i += 2) {
                 DateTime date = DateTime.FromOADate(GlobalBestPosition[i]);
                 TimeSpan time = TimeSpan.FromHours(GlobalBestPosition[i + 1]);
 
-                Event e = events[i];
+                Event e = events[i / 2];
                 foreach (User user in e.Participants) {
                     user.Schedule.Add(new Event { Name = e.Name, Date = date, Time = time });
                 }
--- Test/Test.cs
+++ Test/Test.cs
@@ -8,19 +8,21 @@ public class Test {
     public static void Main() {
         List<Event> events = new List<Event> {
             new Event {
-                Name = "Evento 1",
+                Name = "Event 1",
                 Duration = new TimeSpan(1, 0, 0),
                 Participants = new List<User> {
                     new User {
-                        Name = "João",
+                        Name = "David",
                         Schedule = new List<Event> {
-                            new Event { Name = "Evento A", Date = new DateTime(2022, 12, 1), Time = new TimeSpan(9, 0, 0), Duration = new TimeSpan(1, 0, 0) }
+                            new Event { Name = "Event A", Date = new DateTime(2023, 06 , 1), Time = new TimeSpan(9, 0, 0), Duration = new TimeSpan(1, 0, 0) } ,
+                            new Event { Name = "Event B", Date = new DateTime(2023, 06 , 1), Time = new TimeSpan(10, 0, 0), Duration = new TimeSpan(0, 30, 0) }
                         }
                     },
                     new User {
-                        Name = "Maria",
+                        Name = "Eduardo",
                         Schedule = new List<Event> {
-                            new Event { Name = "Evento C", Date = new DateTime(2022, 12, 3), Time = new TimeSpan(10, 0, 0), Duration = new TimeSpan(1, 0, 0) }
+                            new Event { Name = "Event C", Date = new DateTime(2023, 06 , 3), Time = new TimeSpan(10, 0, 0), Duration = new TimeSpan(1, 0, 0) } ,
+                            new Event { Name = "Event D", Date = new DateTime(2023, 06 , 3), Time = new TimeSpan(11, 0, 0), Duration = new TimeSpan(2, 0, 0) }
                         }
                     }
                 }
@@ -28,18 +30,18 @@ public class Test {
         };
 
         PSOAlias.PSO pso = new PSOAlias.PSO {
-            PopulationSize = 100,
+            PopulationSize = 300,
             Iterations = 1000,
             InertiaCoefficient = 0.5,
             CognitiveCoefficient = 1.0,
             SocialCoefficient = 1.0,
-            GlobalBestPosition = new double[] {}
+            GlobalBestPosition = new double[events.Count * 2]
         };
-        
+
         pso.Run(events);
 
         foreach (User user in events[0].Participants) {
-            Console.WriteLine("Agenda do usuário " + user.Name + ":");
+            Console.WriteLine(user.Name + "` :" + "Schedule");
             foreach (Event e in user.Schedule) {
                 Console.WriteLine(" - " + e.Name + ": " + e.Date.ToShortDateString() + " " + e.Time);
             }
