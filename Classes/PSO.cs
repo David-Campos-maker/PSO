c4eece995ea5012e50f7f16473fb686ab17e4c24
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-08-03 16:30:24 -0300

    Testing

--- Classes/PSO.cs
+++ Classes/PSO.cs
@@ -10,24 +10,18 @@ namespace PSO.Classes {
         public double InertiaCoefficient { get; set; }
         public double CognitiveCoefficient { get; set; }
         public double SocialCoefficient { get; set; }
-        public required double[] GlobalBestPosition { get; set; }
+        public double[] GlobalBestPosition { get; set; } //required
 
         public void Run(List<Event> events) {
             Random random = new();
 
-            int populationSize = PopulationSize;
-            int iterations = Iterations;
-            double inertiaCoefficient = InertiaCoefficient;
-            double cognitiveCoefficient = CognitiveCoefficient;
-            double socialCoefficient = SocialCoefficient;
-
             // Initialize population
-            double[][] population = new double[populationSize][];
-            double[][] velocities = new double[populationSize][];
-            double[] personalBestQuality = new double[populationSize];
-            double[][] personalBestPosition = new double[populationSize][];
+            double[][] population = new double[PopulationSize][];
+            double[][] velocities = new double[PopulationSize][];
+            double[] personalBestQuality = new double[PopulationSize];
+            double[][] personalBestPosition = new double[PopulationSize][];
 
-            for (int i = 0; i < populationSize; i++) {
+            for (int i = 0; i < PopulationSize; i++) {
                 population[i] = new double[events.Count * 2];
                 velocities[i] = new double[events.Count * 2];
                 personalBestPosition[i] = new double[events.Count * 2];
@@ -55,7 +49,6 @@ namespace PSO.Classes {
                 personalBestQuality[i] = double.MaxValue;
             }
 
-            double[] globalBestPosition = new double[events.Count * 2];
             double globalBestQuality = double.MaxValue;
 
             int noImprovementCount = 0;
@@ -65,11 +58,11 @@ namespace PSO.Classes {
             double finalInertiaCoefficient = 0.4;
 
             // Perform optimization iterations
-            for (int iter = 0; iter < iterations; iter++) {
+            for (int iter = 0; iter < Iterations; iter++) {
                 // Check if the PSO is stuck in a local optimum
                 if (noImprovementCount >= noImprovementLimit) {
                     // Restart PSO with a new random population
-                    for (int i = 0; i < populationSize; i++) {
+                    for (int i = 0; i < PopulationSize; i++) {
                         for (int j = 0; j < events.Count; j++) {
                             Event e = events[j];
                             bool isPriority = e.Priority;
@@ -93,18 +86,17 @@ namespace PSO.Classes {
                         personalBestQuality[i] = double.MaxValue;
                     }
 
-                    globalBestPosition = new double[events.Count * 2];
                     globalBestQuality = double.MaxValue;
 
                     noImprovementCount = 0;
                 }
 
                 // Update inertia coefficient
-                inertiaCoefficient = initialInertiaCoefficient - ((initialInertiaCoefficient - finalInertiaCoefficient) * iter / iterations);
+                InertiaCoefficient = initialInertiaCoefficient - ((initialInertiaCoefficient - finalInertiaCoefficient) * iter / Iterations);
 
                 // Select a random subset of the population
-                int sampleSize = populationSize / 2;
-                int[] sampleIndices = Enumerable.Range(0, populationSize).OrderBy(x => random.Next()).Take(sampleSize).ToArray();
+                int sampleSize = PopulationSize / 2;
+                int[] sampleIndices = Enumerable.Range(0, PopulationSize).OrderBy(x => random.Next()).Take(sampleSize).ToArray();
 
                 // Update personal best positions
                 for (int i = 0; i < sampleSize; i++) {
@@ -118,7 +110,7 @@ namespace PSO.Classes {
 
                         if (quality < globalBestQuality) {
                             globalBestQuality = quality;
-                            globalBestPosition = position;
+                            GlobalBestPosition = position;
 
                             noImprovementCount = 0;
                         }
@@ -136,10 +128,10 @@ namespace PSO.Classes {
                         double rp = random.NextDouble();
                         double rg = random.NextDouble();
 
-                        if (globalBestPosition != null) {
-                            velocity[j] = inertiaCoefficient * velocity[j]
-                            + cognitiveCoefficient * rp * (personalBestPos[j] - position[j])
-                            + socialCoefficient * rg * (globalBestPosition[j] - position[j]);
+                        if (GlobalBestPosition != null) {
+                            velocity[j] = InertiaCoefficient * velocity[j]
+                            + CognitiveCoefficient * rp * (personalBestPos[j] - position[j])
+                            + SocialCoefficient * rg * (GlobalBestPosition[j] - position[j]);
 
                             position[j] += velocity[j];
                         }
@@ -150,13 +142,13 @@ namespace PSO.Classes {
             }
 
             // Update the schedule based on the global best position
-            if (globalBestPosition != null) {
-                for (int i = 0; i < globalBestPosition.Length - 1; i += 2) {
+            if (GlobalBestPosition != null) {
+                for (int i = 0; i < GlobalBestPosition.Length - 1; i += 2) {
                     int eventIndex = i / 2;
                     Event e = events[eventIndex];
 
-                    DateTime eventDate = DateTime.FromOADate(globalBestPosition[i]);
-                    TimeSpan eventTime = TimeSpan.FromHours(globalBestPosition[i + 1]);
+                    DateTime eventDate = DateTime.FromOADate(GlobalBestPosition[i]);
+                    TimeSpan eventTime = TimeSpan.FromHours(GlobalBestPosition[i + 1]);
 
                     if (e != null) {
                         if (e.Participants != null) {
