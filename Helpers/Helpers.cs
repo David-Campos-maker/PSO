395cb692ff0ea84c92aca154e5073af1f058f350
David-Campos-maker <davidcamposcruz2016@gmail.com>
2023-09-04 22:02:31 -0300

    objective function restructuring

--- Helpers/Helpers.cs
+++ Helpers/Helpers.cs
@@ -6,27 +6,36 @@ using PSO.Classes;
 
 namespace PSO.Helpers {
      public static class Helper {
-        public static bool IsEventWithinSchedule(DateTime date, TimeSpan eventStart, TimeSpan eventEnd, List<Event> schedule) {
-            if (schedule == null)
+        private static bool CanEventFitInDay(Event newEvent, List<Event> scheduledEvents) {
+            // Sort scheduled events by start time
+            scheduledEvents.Sort((e1, e2) => e1.Time.CompareTo(e2.Time));
+
+            // Check if the new event fits before the first scheduled event
+            if (newEvent.Time + newEvent.Duration <= scheduledEvents[0].Time) {
                 return true;
+            }
 
-            return !schedule.Any(scheduledEvent => {
-                if (scheduledEvent == null)
-                    return false;
+            // Check if the new event fits between two scheduled events
+            for (int i = 0; i < scheduledEvents.Count - 1; i++) {
+                TimeSpan startTime = scheduledEvents[i].Time + scheduledEvents[i].Duration;
+                TimeSpan endTime = scheduledEvents[i + 1].Time;
 
-                DateTime scheduledDate = scheduledEvent.Date.Date;
-                TimeSpan scheduledStartTime = scheduledEvent.Time;
-                TimeSpan scheduledEndTime = scheduledStartTime.Add(scheduledEvent.Duration);
+                if (newEvent.Time >= startTime && newEvent.Time + newEvent.Duration <= endTime) {
+                    return true;
+                }
+            }
 
-                return scheduledDate == date && !(eventEnd <= scheduledStartTime || eventStart >= scheduledEndTime);
-            });
+            // Check if the new event fits after the last scheduled event
+            if (newEvent.Time >= scheduledEvents[scheduledEvents.Count - 1].Time + scheduledEvents[scheduledEvents.Count - 1].Duration) {
+                return true;
+            }
+
+            // The new event does not fit on the day
+            return false;
         }
 
         public static double ObjectiveFunction(double[] position, List<Event> events) {
             double quality = 0.0;
-
-            DateTime currentDate = DateTime.Now.Date;
-            DateTime maxFutureDate = currentDate.AddMonths(1).Date;
 
             // Calculate the quality of each event
             for (int i = 0; i < events.Count; i++) {
@@ -39,35 +48,26 @@ namespace PSO.Helpers {
                 DateTime eventDate = DateTime.FromOADate(position[i * 2]);
                 TimeSpan time = TimeSpan.FromHours(position[i * 2 + 1]);
 
-                if (eventDate < currentDate) {
-                    eventQuality += 10000.0; // Penalize invalid dates
+                // Check if the current event overlaps with any other event
+                for (int j = 0; j < events.Count; j++) {
+                    if (i == j) continue; // Skip the current event
+
+                    Event otherEvent = events[j];
+                    if (otherEvent == null) continue;
+
+                    DateTime otherEventDate = DateTime.FromOADate(position[j * 2]);
+                    TimeSpan otherEventStartTime = TimeSpan.FromHours(position[j * 2 + 1]);
+                    TimeSpan otherEventEndTime = otherEventStartTime.Add(otherEvent.Duration);
+
+                    if (eventDate == otherEventDate && !(time >= otherEventEndTime || time + e.Duration <= otherEventStartTime)) {
+                        eventQuality += 10000.0; // Apply penalty for overlapping event
+                    }
                 }
 
-                if (e.Participants != null) {
-                    foreach (User user in e.Participants.Where(user => user != null && user.Schedule != null)) {
-                        // Check if the current event overlaps with any scheduled event
-                        foreach (Event scheduledEvent in user.Schedule) {
-                            DateTime scheduledDate = scheduledEvent.Date.Date;
-                            TimeSpan scheduledStartTime = scheduledEvent.Time;
-                            TimeSpan scheduledEndTime = scheduledStartTime.Add(scheduledEvent.Duration);
-
-                            if (eventDate == scheduledDate && !(time >= scheduledEndTime || time + e.Duration <= scheduledStartTime)) {
-                                eventQuality += 10000.0; // Apply penalty for overlapping event
-                            }
-                        }
-
-                        // Check if the current event is within the user's schedule
-                        if (!IsEventWithinSchedule(eventDate, time, time.Add(e.Duration), user.Schedule)) {
-                            eventQuality += 10000.0; // Apply a large penalty for event not being within the schedule
-                        }
-
-                        // Check if the current event fits within the user's work day
-                        TimeSpan workDayStart = new(7, 0, 0);
-                        TimeSpan workDayEnd = new(18, 0, 0);
-                        if (time + e.Duration > workDayEnd || time + e.Duration < workDayStart) {
-                            eventQuality += 10000.0; // Apply a large penalty for event not fitting within the user's work day
-                        }
-                    }
+                // Check if the current event fits within the day
+                List<Event> eventsOnSameDay = events.Where(ev => ev != null && DateTime.FromOADate(position[events.IndexOf(ev) * 2]) == eventDate).ToList();
+                if (!CanEventFitInDay(e, eventsOnSameDay)) {
+                    eventQuality += 10000.0; // Apply penalty for event not fitting within the day
                 }
 
                 // Add the quality of the current event to the total quality
